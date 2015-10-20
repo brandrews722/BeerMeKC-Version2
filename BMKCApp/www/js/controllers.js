6e515576dec6629f9f61fc0ddfd0d436e440d3d9
jdlars <jlarson.nwciowa@gmail.com>
2015-10-20 00:37:03 -0500

    Added KC Brewery List to browseAll view

--- BMKCApp/www/js/controllers.js
+++ BMKCApp/www/js/controllers.js
@@ -1,71 +1,80 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function() {
+      $scope.modal.hide();
+    };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Open the login modal
+    $scope.login = function() {
+      $scope.modal.show();
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+      console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function() {
+        $scope.closeLogin();
+      }, 1000);
+    };
+  })
 
   .controller('BreweriesCtrl', function($scope, $http) {
-    $http.get('https://api.brewerydb.com/v2/locations/?key=fd038434276f4a9e7d6a19ee2d8aa5b5&locality=Kansas%20City').then(function(resp) {
-      console.log('Success', resp.data);
+    var baseUrl = 'https://api.brewerydb.com/v2/';
+    var breweryDBKey = '?key=fd038434276f4a9e7d6a19ee2d8aa5b5';
+    $http.get(baseUrl + 'locations/' + breweryDBKey + '&locality=Kansas%20City').then(function (resp) {
+      //console.log('Success', resp.data);
+      $scope.breweries = [];
+      angular.forEach(resp.data.data, function (item) {
+        //console.log(item.breweryId);
+        if ($scope.breweries.indexOf(item.brewery.name) == -1) {
+          $scope.breweries.push(item.brewery.name);
+        }
 
-      angular.forEach(resp.data.data, function(item) {
-        console.log(item.breweryId);
         getBreweryById(item.breweryId);
+        getBeersForBrewery(item.breweryId);
       })
-    }, function(err) {
+      console.log("Breweries for view: " + $scope.breweries);
+    }, function (err) {
       console.error('ERR', err);
     })
 
-    $scope.breweries = [
-      { name: 'Big Rip', id: 1 },
-      { name: 'Cinder Block', id: 2 },
-      { name: 'Boulevard', id: 3 },
-      { name: 'Torn Label', id: 4 },
-      { name: 'Stockyards', id: 5 },
-      { name: 'Eye For An Eye', id: 6 }
-    ];
+
 
     function getBreweryById(id) {
-      $http.get('http://api.brewerydb.com/v2/brewery/' + id + '?key=fd038434276f4a9e7d6a19ee2d8aa5b5').then(function(resp) {
-        console.log('Success - Brewery with id ' + id, resp.data);
-      }, function(err) {
+      $http.get('http://api.brewerydb.com/v2/brewery/' + id + '?key=fd038434276f4a9e7d6a19ee2d8aa5b5').then(function (resp) {
+        //console.log('Success - Brewery with id ' + id, resp.data);
+      }, function (err) {
+        console.error('ERR', err);
+      });
+    }
+
+    function getBeersForBrewery(id) {
+      $http.get('http://api.brewerydb.com/v2/brewery/' + id + '/beers/' + breweryDBKey).then(function (resp) {
+        console.log('Success - Brewery with id ' + id + ' has the following beers: ', resp.data);
+      }, function (err) {
         console.error('ERR', err);
       });
     }
